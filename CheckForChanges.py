e3925383fed759186bc27af99ba4be9fe58fe132
NemanjaMiloicic <nemanjamiloicice24@gmail.com>
2026-02-20 20:16:59 +0100

    window size

--- CheckForChanges.py
+++ CheckForChanges.py
@@ -70,7 +70,6 @@ def take_screenshot(driver, element, image_path = IMAGE_PATH):
     highlight_element(driver, element)
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     driver.set_window_size(1920,1080)
-    driver.execute_script("arguments[0].scrollIntoView();", element)
     driver.save_screenshot(f"{image_path}_{timestamp}.png")
 
 def remove_previous_screenshots(image_path = IMAGE_PATH):
